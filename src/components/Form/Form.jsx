dd198fa8733e287da1fc4c221f30e37867182529
rigobertoalag <a20150338@utem.edu.mx>
2022-04-04 17:48:29 -0500

    Remove innesary object in Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,12 +5,9 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
-
-  // console.log(watch("example"));
 
   return (
     <form
